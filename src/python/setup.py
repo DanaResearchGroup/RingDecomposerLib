6da57ef8a359466f57a9d6c4640982d410d50485
Alon Grinberg Dana <alon@technion.ac.il>
2025-05-16 13:37:18 +0300

    updated setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,23 +2,27 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
 
-# absolute path to RDL root
 RDL_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 
 extensions = [
     Extension(
         name='py_rdl.wrapper.DataInternal',
         sources=['py_rdl/wrapper/DataInternal.pyx'],
-        include_dirs=[os.path.join(RDL_ROOT, 'src', 'RingDecomposerLib')],
-        extra_objects=[os.path.join(RDL_ROOT, 'build', 'src', 'RingDecomposerLib', 'libRingDecomposerLibStatic.a')],
-        language='c++'
+        include_dirs=[
+            os.path.join(RDL_ROOT, 'src', 'RingDecomposerLib'),
+            os.path.join(RDL_ROOT, 'build', 'src', 'RingDecomposerLib'),
+            os.path.join(os.path.dirname(__file__), 'py_rdl', 'wrapper'),  # crucial for .pxd
+        ],
+        extra_objects=[
+            os.path.join(RDL_ROOT, 'build', 'src', 'RingDecomposerLib', 'libRingDecomposerLibStatic.a')
+        ],
+        language='c++',
     )
 ]
 
 setup(
     name='py_rdl',
     packages=['py_rdl', 'py_rdl.wrapper'],
-    package_dir={'py_rdl': '.'},
     package_data={'py_rdl.wrapper': ['*.pyx', '*.pxd']},
     ext_modules=cythonize(extensions, language_level=3),
     zip_safe=False
